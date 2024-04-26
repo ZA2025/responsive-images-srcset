a3fead3db0ae344c7c8eb3b3c1a400309c7879d6
ZA2025 <zainabal2025@gmail.com>
2024-04-26 21:05:34 +0100

    update the image sizes

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ const albums = [
     // Album with 3 columns
     responsiveImgAttrs2(
         springArray.map(src => `placeholders/${src}`),
-        "(min-width: 1600px) 386px, (min-width: 1200px) calc((1200px - 40px) / 3), (min-width: 768px) calc(((768px - 40px) - 40px) / 3), calc(100vw - 40px)",
+        "(min-width: 1600px) 386px, (min-width: 1200px) calc((1200px - 40px) / 3), (min-width: 768px) calc(((768px - 40px) - 40px) / 3), calc(((100vw - 40px) - 20px) / 2)",
         [600, 800, 1200, 1600],
         800,
         "imageSpring",
